3fffff0008dd01a484d5009c0ff2bd7c0c6c34d6
ThreeManager785 <leimeichun50@gmail.com>
2026-02-08 18:47:36 +1000

    Initial Commit

---
+++ Epub-Tr/Epub_TrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Epub_TrApp.swift
+//  Epub-Tr
+//
+//  Created by ThreeManager785 on 2026/2/8.
+//
+
+import SwiftUI
+
+@main
+struct Epub_TrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
